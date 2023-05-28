8f80cf454604b270f7c411228d3236c27b355051
P4ndaM1x <P4ndaM1x@users.noreply.github.com>
2023-05-28 17:32:48 +0200

    first implementation of data extraction
    `dataparser.py` script was created to extract data from raw csv files to dictionary objects; they are saved to seperated files and used in `data_parser_test.ipynb`.
    
    `Budżet państwa_miesięczne.csv` and `Koniunktura konsumencka i gospodarcza_miesięczne.csv` are not yet supported due to unique file structures

---
+++ data_parser.py
@@ -0,0 +1,55 @@
+__author__ = '@P4ndaM1x, Michał Rutkowski'
+
+import pandas as pd
+import numpy as np
+
+rawdata_path = './macroeconomic_data/'
+rawdata_filename_suffix = '_miesięczne.csv'
+indicators_labels = {
+    0: 'Wskaźniki cen',  # dane od 2000 roku
+    1: 'Budownictwo',  # dane od 2000 roku
+    2: 'Budżet państwa',  # dane od 2000 roku (od początku roku do końca okresu!)
+    3: 'Handel wewnętrzny',  # dane od 2006 roku
+    4: 'Handel zagraniczny',  # dane od 2000 roku
+    5: 'Koniunktura konsumencka i gospodarcza',  # dane od 2000 roku
+    6: 'Przemysł',  # dane od 2005 roku
+    7: 'Rynek pracy',  # dane od 2000 roku
+    8: 'Transport',  # dane od 2011 roku
+    9: 'Wynagrodzenia i świadczenia społeczne'  # dane od 2000 roku
+}
+indicator_offset = -100.
+years_range = (2000, 2022)
+
+indicators_list = []
+subindicators_labels = {}
+for indicator_id, indicator_name in indicators_labels.items():
+
+    # TODO szczególna implementacja ze względu na unikalną strukturę plików z surowymi danymi
+    if indicator_id in [2, 5]:
+        continue
+
+    filename = rawdata_path + indicator_name + rawdata_filename_suffix
+    df = pd.read_csv(filename, sep=';', header=[0, 1])
+    interesting_rows = df[df['Jednostka']['Unnamed: 2_level_1'] == 'okres poprzedni=100']
+
+    labels = list(interesting_rows.iloc[:, [0, 1]].to_dict().values())
+    subindicators_labels[indicator_id] = {date: '; '.join([str(d[date]) for d in labels])
+                                          for date in labels[0]}
+
+    interesting_columns = [column for column in df.columns
+                           if column[0].isdigit() and years_range[0] <= int(column[0]) <= years_range[1]]
+    data = interesting_rows[interesting_columns].to_dict()
+
+    data = {tuple(map(int, k)):
+                {indicator_id: {key: None if value == '.' else float(str(value).replace(',', '.')) + indicator_offset
+                                for key, value in d.items()}}
+            for k, d in data.items()}
+
+    indicators_list.append(data)
+
+parsed_data = {date: {k: v for d in indicators_list if date in d for k, v in d[date].items()}
+               for date in indicators_list[0]}
+
+np.save(rawdata_path + 'indicators_labels.npy', indicators_labels)
+np.save(rawdata_path + 'subindicators_labels.npy', subindicators_labels)
+np.save(rawdata_path + 'parsed_data.npy', parsed_data)
